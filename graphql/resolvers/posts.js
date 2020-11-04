a53ab6ca84bf0dc82820539df10308f642a34c92
InvincibleNobita <invincible.manish1417@gmail.com>
2020-11-04 19:23:18 +0530

    Registre & login users

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,5 +1,7 @@
 const Post = require('../../models/Post');
 
+//this file contains all the queries format strictly related to posts we can ask from our database using graphql-schema'
+
 module.exports = {
     Query: {
         async getPosts() {
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,6 +1,9 @@
 const { gql } = require('apollo-server');
 
+//using '!' this doesnt mean that the users has to fill this details. 
+//It just means that smehow we have to return these to our resolvers.
 
+//input is a different type of "type", which is actually provied as an argument to another type
 module.exports = gql`
     type Post{
         id: ID!
@@ -8,7 +11,26 @@ module.exports = gql`
         createdAt: String!
         username: String!
     }
+    type User{
+        id: ID!
+        email: String!
+        token: String!
+        username: String!
+        createdAt: String!
+        
+    }
+    
+    input RegisterInput{
+        username: String!
+        password: String!
+        confirmPassword: String!
+        email: String!
+    }
     type Query{
         getPosts: [Post]
+    }
+    type Mutation{
+        register(registerInput: RegisterInput): User!
+        login(username: String!, password: String!): User!
     }
 `;
